109168a73af859899ff8179a7b20e2245ae76d2f
ypconstante <ypconstante@gmail.com>
2017-09-16 21:55:50 -0300

    página de 404

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const Koa = require('koa');
 const koaStatic = require('koa-static');
+const fs = require('fs');
 const app = new Koa();
 
 app.use(koaStatic('target'));
+
+app.use(async (ctx, next) => {
+    await next();
+    if (ctx.status === 404) {
+        ctx.type = 'html';
+        ctx.body = fs.createReadStream('./target/404.html');
+    }
+});
 
 app.listen(3000);
